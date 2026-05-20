e5de9f0ba8d8839e16aaa7aa5abfa16dfb75cca3
gischellatte <40295535+gischellatte@users.noreply.github.com>
2026-05-20 10:53:19 +1000

    Update QuantityCounter.test.jsx

--- src/components/QuantityCounter.test.jsx
+++ src/components/QuantityCounter.test.jsx
@@ -5,43 +5,30 @@ import QuantityCounter from './QuantityCounter'
 
 describe('Check if it calls onChange with the right quantity', ()=>{
   it('Should update quantity when we click on the + button', async ()=>{
-    //Step 1 - Declaring
+    
     const customer = userEvent.setup();  
-
-    //we assign vi.fn() to onChange because it is passed from the property and we want to monitor this. But since it is not a built-in/an existing function, we cant use vi.spyOn()
+  
     const onChange = vi.fn();
 
-    //Step 2 - Render Component
     render(<QuantityCounter counter={3} onChange={onChange}/>);
 
-    //Step 3 - Take the UI elements
     const incrButton = screen.getByText("+");
   
-    //Step 4 - Simulate user actions
-    //await because it could have an async DOM update or waiting on effects such as browser, so better use await for userEvent.click() 
     await customer.click(incrButton);
 
-    //Step 5 - Assertion
-    //toHaveBeenCalled() sends a true or false
-    //toHaveBeenCalledWith(4) checks if it sends a value of 4?
     expect(onChange).toHaveBeenCalledWith(4);
   })
 
   it('Should update quantity when we click on the - button', async ()=>{
-    //Step 1 - Declaring
     const customer2 = userEvent.setup();  
     const onAmend = vi.fn();
 
-    //Step 2 - Render Component
     render(<QuantityCounter counter={3} onChange={onAmend}/>);
 
-    //Step 3 - Take the UI elements
     const decrButton = screen.getByText("-");
 
-    //Step 4 - Simulate user actions
     await customer2.click(decrButton);
 
-    //Step 5 - Assertion
     expect(onAmend).toHaveBeenCalledWith(2);
   })
 
@@ -53,7 +40,6 @@ describe('Check if it calls onChange with the right quantity', ()=>{
 
     const decrButton = screen.getByText("-");
 
-    //await customer3.click(decrButton) is not used, because there is no user action here. In this case there is no user action.
     
     expect(decrButton).toBeDisabled();
 
