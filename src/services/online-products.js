c0203b38b33359cd56aa0a6f5c6aa211b0dbc26c
gischellatte <40295535+gischellatte@users.noreply.github.com>
2026-05-20 10:55:57 +1000

    Update online-products.js

--- src/services/online-products.js
+++ src/services/online-products.js
@@ -3,18 +3,11 @@ import { collection, doc, getDoc,  getDocs, onSnapshot
 import { db } from '../config/firestore-config';
 
 export const retrieveProducts = async (category, id) => {
-    
-     // Firestore path: ecommerce-products / onlineCatalogue / <category> / <id>
-     //Path: ecommerce (collection)/onlineCatalogues(document)/category/productID
-
-     //Rule: odd = collection, even = document
-     //even number of params after db -> doc(db, 'a', 'b', 'c', 'd'), d is a document ( we refer to a document in a doc). so 'd' is a document if that points to a specific document inside collection c
-    
-    const docRef = doc(db, 'ecommerce-products', //If a function uses doc() -> it must receive IDs, if a page lists many items -> it must use getDocs() and arrays
+       const docRef = doc(db, 'ecommerce-products', 
         'onlineCatalogue', 
         category, 
-        id);//refers to a specific document
-    const snapshot  = await getDoc(docRef); //async function that fetches data
+        id);
+    const snapshot  = await getDoc(docRef);
 
 
     if(!snapshot.exists()){
@@ -24,19 +17,17 @@ export const retrieveProducts = async (category, id) => {
 }
 
 export const retrieveAllProducts = async () => {
- 
-    //category for dynamic selection
+
+    
     let prodCategories = [ "almond", "jam", "riceCake", "yakgwa"];
     let fullProducts=[];
     
-    //can't put categories as a plaintext here, because there are multiple categories
-    //Rule: retrieving all products mean we need to specify the categories, because the system cannot tell how many categories we have and we must tell them
-    for (const pc of prodCategories){ //for of loop works with async await, for each loop does not
+    
+    for (const pc of prodCategories){ 
     const allProdsRef = collection(db, 'ecommerce-products', 'onlineCatalogue', pc);
     const productsDocs = await getDocs(allProdsRef);
     const prods =  productsDocs.docs.map((doc) => ({ id: doc.id, ...doc.data(), category:pc })); //we need to add the id because it does not contain ID
-    //we need data() in ...doc.data() because doc is a DocumentSnapshot object. it is not directly mutable, which means we cannot spread it. 
-    //doc.data() returns a plain JS object containing document data, which is mutable and can be spread to a new object
+   
     fullProducts =[...fullProducts, ...prods];
    
   }    
