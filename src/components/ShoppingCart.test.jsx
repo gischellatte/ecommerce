f1d68bc5c582f745656e6d493b48d65e92fe87f1
gischellatte <40295535+gischellatte@users.noreply.github.com>
2026-05-20 10:53:56 +1000

    Update ShoppingCart.test.jsx

--- src/components/ShoppingCart.test.jsx
+++ src/components/ShoppingCart.test.jsx
@@ -6,8 +6,8 @@ import ShoppingCart from './ShoppingCart';
 describe('Test the summing up ability of the Shopping cart', ()=>{
     it('Should sum up the total items',()=>{
     render(<ShoppingCart allTotal={15.009}/>);
-    //If we use getByText("Total"), you may need to exactly match it with <p>Total: $</p>
-    const totalAmount = screen.getByText(/Total/);// getByDisplayValue() is used for <input> and <textarea>
+
+    const totalAmount = screen.getByText(/Total/);
     expect(totalAmount).toHaveTextContent(/15\.01/);
     })
     
